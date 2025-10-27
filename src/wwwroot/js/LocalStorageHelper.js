cfbba482e295576ccee235c7e46f622050712e2e
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2025-10-28 08:03:22 +0900

    タブを閉じる前のデータ更新をサポートするために、スクリプトを追加。

---
+++ src/wwwroot/js/LocalStorageHelper.js
@@ -0,0 +1,9 @@
+window.onbeforeunload = function () {
+    let connectedClients = localStorage.getItem("connectedClients");
+    if (connectedClients == null) {
+        connectedClients = "0";
+    }
+    let count = parseInt(connectedClients);
+    count = Math.max(0, count - 1);
+    localStorage.setItem("connectedClients", count.toString());
+}
