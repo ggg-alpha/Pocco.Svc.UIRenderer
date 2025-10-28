94caada992d5323beade00fdf14dd5630aadf4e2
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2025-10-29 08:25:13 +0900

    ログインまたは登録の時にemailとpasswordが正しく入力されているかをチェックするように変更。

--- src/Pages/Login.razor.cs
+++ src/Pages/Login.razor.cs
@@ -8,8 +8,37 @@ partial class Login : ComponentBase
     private NavigationManager NavigationManager { get; set; } = null!;
 
     private string email = string.Empty;
+    private string emailError = string.Empty;
+
     private string password = string.Empty;
+    private string passwordError = string.Empty;
+
     private bool isLoading = false;
+    private bool hasEmailError = false;
+    private bool hasPasswordError = false;
+
+    private async Task OnEmailInputChange(ChangeEventArgs e)
+    {
+        var value = e.Value?.ToString() ?? string.Empty;
+
+        var isValid = string.IsNullOrWhiteSpace(value);
+        isValid = !(value.Contains('@') && value.Contains('.'));
+
+        emailError = isValid ? "Email is required." : string.Empty;
+        hasEmailError = isValid;
+
+        await Task.CompletedTask;
+    }
+    private async Task OnPasswordInputChange(ChangeEventArgs e)
+    {
+        var value = e.Value?.ToString() ?? string.Empty;
+
+        var empty = string.IsNullOrWhiteSpace(value);
+        passwordError = empty ? "Email is required." : string.Empty;
+        hasPasswordError = empty;
+
+        await Task.CompletedTask;
+    }
 
     private void HandleLogin()
     {
