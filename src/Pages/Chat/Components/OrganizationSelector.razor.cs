c7861094910561d8ee1d46c192273b4f9616fc06
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2025-11-11 21:42:04 +0900

    組織選択ボタンで、Modalを開けるように変更。

--- src/Pages/Chat/Components/OrganizationSelector.razor.cs
+++ src/Pages/Chat/Components/OrganizationSelector.razor.cs
@@ -4,5 +4,21 @@ namespace Pocco.Client.Web.Pages.Chat.Components;
 
 public partial class OrganizationSelector : ComponentBase
 {
-    //
+    [Parameter] public Page ParentPage { get; set; } = null!;
+    protected override async Task OnInitializedAsync()
+    {
+        ParentPage.OrgSelectorRef = this;
+
+        await base.OnInitializedAsync();
+    }
+
+    private async Task OnOpenOrganizationSelectorClicked()
+    {
+        var eventModel = new InPageEventModel
+        {
+            EventType = InPageEventTypes.OpenModal
+        };
+
+        await ParentPage.InvokeEventAsync(eventModel);
+    }
 }
