c35bbdea2b58981f2d83f5abddf053c78765406b
STanoue19 <fxb4zffhb@i.softbank.jp>
2026-01-22 18:54:31 +0900

    JavaScript 連携処理（JSInterop）を実装。

---
+++ src/Pages/DrawingTool/DrawingTool.razor.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using Pocco.APIClient.Core;
+using Pocco.Client.Web.Pages.DrawingTool.Components;
+using Pocco.Client.Web.Services;
+using Pocco.Libs.Protobufs.CoreAPI.Services;
+
+namespace Pocco.Client.Web.Pages.DrawingTool.Components;
+
+public partial class DrawingTool : ComponentBase {}
